d17c8f1968e8f824b24841d980a807febba12245
Magenta <paredesponcemagenta@gmail.com>
2025-07-17 12:56:50 -0500

    Enviando funcionalidad en host

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
-$host = "magenta.c7eoowk848fz.us-east-2.rds.amazonaws.com";
-$user = "admin";
-$pass = "5wpMSZQcOvKuR81S734D";
-$db   = "MAGENTA";
+$host = "sql312.infinityfree.com";
+$user = "if0_39496179";
+$pass = "1Lk845CRKxT";  
+$db   = "if0_39496179_magenta";
 
 $conexion = new mysqli($host, $user, $pass, $db);
+
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
